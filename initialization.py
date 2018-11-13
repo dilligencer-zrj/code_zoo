256ae68cfa73bd2ce9119b69819501f28a13ed8f
Zhao Rongjian <31402810+dilligencer-zrj@users.noreply.github.com>
2018-11-13 19:16:04 +0800

    Create initialization.py

---
+++ initialization.py
@@ -0,0 +1,12 @@
+
+def weights_init_xavier(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if classname.find('Conv') != -1:
+        init.xavier_normal(m.weight.data, gain=1)
+    elif classname.find('Linear') != -1:
+        init.xavier_normal(m.weight.data, gain=1)
+    elif classname.find('BatchNorm2d') != -1:
+        init.uniform(m.weight.data, 1.0, 0.02)
+        init.constant(m.bias.data, 0.0)
+# net.apply(weights_init_xavier)
