615546deb642ed5b2fbccef1ccba08cd2c7f2900
Zhao Rongjian <31402810+dilligencer-zrj@users.noreply.github.com>
2018-11-14 10:12:43 +0800

    Update initialization.py

--- initialization.py
+++ initialization.py
@@ -9,4 +9,51 @@ def weights_init_xavier(m):
     elif classname.find('BatchNorm2d') != -1:
         init.uniform(m.weight.data, 1.0, 0.02)
         init.constant(m.bias.data, 0.0)
+
+# usage:
 # net.apply(weights_init_xavier)
+
+
+from torch import nn
+
+
+class Initializer:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def initialize(model, initialization, **kwargs):
+
+        def weights_init(m):
+            if isinstance(m, nn.Conv2d):
+                initialization(m.weight.data, **kwargs)
+                try:
+                    initialization(m.bias.data)
+                except:
+                    pass
+
+            elif isinstance(m, nn.Linear):
+                initialization(m.weight.data, **kwargs)
+                try:
+                    initialization(m.bias.data)
+                except:
+                    pass
+
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1.0)
+                m.bias.data.fill_(0)
+
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1.0)
+                m.bias.data.fill_(0)
+
+        model.apply(weights_init)
+        
+# usage
+# net = Model()  # instantiate the model
+
+# to apply xavier_uniform:
+# Initializer.initialize(model=net, initialization=init.xavier_uniform, gain=init.calculate_gain('relu'))
+
+# or maybe normal distribution:
+# Initializer.initialize(model=net, initialization=init.normal, mean=0, std=0.2)
